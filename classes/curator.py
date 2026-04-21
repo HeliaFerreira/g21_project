4de5d680e3f29678ad5daf61496aa29b520550b4
alivongue <up202506781@g.uporto.pt>
2026-04-21 13:41:31 +0100

    Update curator.py

--- classes/curator.py
+++ classes/curator.py
@@ -1,9 +1,8 @@
 """
 @author: António Brito / Carlos Bragança (2025)
-#objective: class Customer_login
-"""
-# Class Utente
-# import datetime
+#objective: class Product
+"""""
+# Class Curator
 # Import the generic class
 from classes.gclass import Gclass
 
@@ -13,22 +12,21 @@ class Curator(Gclass):
     pos = 0
     sortkey = ''
     # class attributes, identifier attribute 'id' must be the first on the list
-    att = ['_id','_extra_info','_specialty']
+    att = ['_id','_extra_info','_id_museum','_id_specialty']
     # Class header title
-    header = 'Curators'
+    header = 'Curator'
     # field description for use in, for example, input form
-    des = ['Id_Curator','Extra_Info','Specialty']
+    des = ['Id','Extra_Info','Id_Museum','Id_Specialty']
     # Constructor: Called when an object is instantiated
-    def __init__(self,id,extra_info,specialty):
+    def __init__(self, id, extra_info, id_museum, id_specialty):
         super().__init__()
         # Object attributes
         id = Curator.get_id(id)
         self._id = id
         self._extra_info = extra_info
-        self._specialty = specialty
-        # self._telefone = telefone
-        # self._data_nascimento = datetime.date.fromisoformat(data_nascimento)
-        # Add the new object to the Customer's list
+        self._id_museum = id_museum
+        self._id_specialty = id_specialty 
+        # Add the new object to the Curator list
         Curator.obj[id] = self
         Curator.lst.append(id)
     # Object properties
@@ -36,32 +34,17 @@ class Curator(Gclass):
     @property
     def id(self):
         return self._id
-    # name property getter method
+    # extra_info property getter method
     @property
     def extra_info(self):
         return self._extra_info
-    # name property setter method
-    # @nome.setter
-    # def nome(self, nome):
-    #     self._nome = nome
-    # # address property getter method
+
+    # id_museum property getter method
     @property
-    def specialty(self):
-        return self._specialty
-    # address property setter method
-    # @morada.setter
-    # def morada(self, morada):
-    #     self._morada = morada
-    # phone property getter method
-    # @property
-    # def telefone(self):
-    #     return self._telefone
-    # # phone property setter method
-    # @telefone.setter
-    # def telefone(self, telefone):
-    #     self._telefone = telefone 
-    
-    # @property 
-    # def data_nascimento(self):
-    #     return self._data_nascimento 
-    
+    def id_museum(self):
+        return self._id_museum
+
+    # id_specialty property getter method
+    @property
+    def id_specialty(self):
+        return self._id_specialty
