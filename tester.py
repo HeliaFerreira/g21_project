4f19259fafb3354cba1caae989e27935b3e109b6
HeliaFerreira <up202507657@g.uporto.pt>
2026-05-19 14:47:53 +0100

    update tester

--- tester.py
+++ tester.py
@@ -13,15 +13,15 @@ from classes.visitors import Visitors
 
 
 # Reads the classes info
-Curator.read('data/database.db')
 Exhibit.read('data/database.db')
 Museum.read('data/database.db')
 Specialty.read('data/database.db') 
 Visitors.read('data/database.db')
+Curator.read('data/database.db')
 
 
 #creates 3 museums
-
+"""
 m1 = Museum(501, "M1")
 m2 = Museum.from_string("502;M2")
 m3 = Museum(503,"M3")
@@ -35,8 +35,10 @@ if m3.id not in Museum.lst:
 
 for id in Museum.lst:
     print(Museum.obj[id])
+"""
 
 #creates 2 specialties 
+"""
 s1 = Specialty(1,"Renascimento")
 s2 = Specialty.from_string("2;Surrealismo")
 if s1.id not in Specialty.lst:
@@ -46,9 +48,11 @@ if s2.id not in Specialty.lst:
 
 for id in Specialty.lst:
     print(Specialty.obj[id]) 
+"""
+
 
 #creates 2 exhibits 
-
+"""
 e1 = Exhibit(1001,"2025-02-11","Creation","cat") 
 e2 = Exhibit.from_string("1002;2024-02-11;Patience;Saint")
 
@@ -58,8 +62,11 @@ if e2.id not in Exhibit.lst:
     Exhibit.insert(e2.id)
 for id in Exhibit.lst:
     print(Exhibit.obj[id]) 
+"""
+
 
 #creates a curator 
+"""
 for id in Curator.lst:
     print(Curator.obj[id]) 
 
@@ -70,8 +77,11 @@ if c1.id not in Curator.lst:
     Curator.insert(c1.id)
 if c2.id not in Curator.lst:
     Curator.insert(c2.id)
+"""
+
 
 #creates 2 visitors 
+"""
 v1 = Visitors(9001, 1001, 501, "2025-12-31", 500)
 v2 = Visitors.from_string("9002;1002;502;2025-12-31;850")
 
@@ -83,7 +93,7 @@ if v2.id not in Visitors.lst:
 for id in Visitors.lst:
     print(Visitors.obj[id])
 
-
+"""
 
 """
 if len(Customer.lst) == 0:
