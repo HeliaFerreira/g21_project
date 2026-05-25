01bab625065fd38b959dc98f2425c7261d011386
HeliaFerreira <up202507657@g.uporto.pt>
2026-05-25 11:59:53 +0100

    continuação flask

--- g21_flask/testerg21_flask.py
+++ g21_flask/testerg21_flask.py
@@ -5,14 +5,29 @@ Created on Tue May 19 15:30:12 2026
 @author: up202506781
 """
 
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def bem_vindo():
-    return "<h1>Bem-vindo!<h1>"
+    return "<h1>Bem-vindo!</h1>"
+
+
+@app.route("/hello")
+def hello_world():
+    data = datetime.datetime.today()
+    return render_template("hello.html", data = data)
+
+if __name__ == '__main__':
+    app.run() 
+    
+@app.route("/about")
+def about():
+    return render_template("about.html") 
+
 
 if __name__ == '__main__':
     app.run()
