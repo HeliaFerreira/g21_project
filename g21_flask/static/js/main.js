bb819cef91b5918b058a8a0f99d192ba10645753
martimlucas <up202508190@up.pt>
2026-06-07 20:56:18 +0100

    Update main.js

--- g21_flask/static/js/main.js
+++ g21_flask/static/js/main.js
@@ -1,16 +1,16 @@
-// Esperar que a página carregue completamente
+
 document.addEventListener("DOMContentLoaded", function() {
     
-    // Agarrar o botão pelo ID
+
     let botaoGrafico = document.getElementById("btn-grafico");
 
     if (botaoGrafico) {
         botaoGrafico.addEventListener("click", function() {
-            // Efeito fixe: muda o texto do botão quando clicas
+
             botaoGrafico.innerHTML = "Loading data... ⏳";
             botaoGrafico.style.transform = "scale(0.95)";
             
-            // Espera meio segundo (para se ver a animação) e redireciona para o gráfico
+
             setTimeout(function() {
                 window.location.href = "/ranking-exposicoes";
             }, 500);
